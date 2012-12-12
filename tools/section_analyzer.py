8093effa4b8d568ce1e1c180fca43165a066f2ab
Hubert Hesse <github@hubertscorner.de>
2012-12-12 15:50:25 +0200

    Skript mit Parametern

--- tools/section_analyzer.py
+++ tools/section_analyzer.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import markdown
 import codecs
+from optparse import OptionParser
 
-input_file = codecs.open("in.md", mode="r", encoding="utf8")
+parser = OptionParser()
+parser.add_option("-o", "--output-file", dest="output_file")
+parser.add_option("-i", "--input-file", dest="input_file")
+(options, args) = parser.parse_args()
+
+input_file = codecs.open(options.input_file, mode="r", encoding="utf8")
 md = markdown.Markdown(extensions=['sectionparser'])
 
-output_file = codecs.open("out.html", "w", encoding="utf8")
+output_file = codecs.open(options.output_file, "w", encoding="utf8")
 output_file.write("""<!DOCTYPE html>
 <html>
 <head>
